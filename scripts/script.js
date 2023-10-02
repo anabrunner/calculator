5578d95403bc1cf0c91de022202a0a59abbb4cad
anabrunner <126997850+anabrunner@users.noreply.github.com>
2023-10-02 10:12:19 -0700

    Add button selectors and function to update current screen.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 // JavaScript for Calculator
+
+const lastScreen = document.getElementById("lastScreen");
+const currentScreen = document.getElementById("currentScreen");
+const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
+const divideButton = document.getElementById("divide");
+const multiplyButton = document.getElementById("multiply");
+const subtractButton = document.getElementById("subtract");
+const addButton = document.getElementById("add");
+const equalButton = document.getElementById("equal");
+const numButtons = document.querySelectorAll(".buttons > .btn.num");
+
+numButtons.forEach((button) => {
+  button.addEventListener("click", updateScreen)
+});
+
+function updateScreen() {
+  currentScreen.innerText += this.id;
+};
